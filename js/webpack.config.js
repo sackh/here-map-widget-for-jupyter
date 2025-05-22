62139da959e102b86d832d89d2b82bf466276168
Keisuke Kishida <keisuke.kishida@here.com>
2025-05-22 23:42:57 +0900

    add events polyfill (2)
    Signed-off-by: Kento Tarui <kento.tarui@here.com>

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -28,7 +28,7 @@ var resolve = {
         "crypto": require.resolve("crypto-browserify"),
         "buffer": require.resolve("buffer/"),
         "stream": require.resolve("stream-browserify"),
-        "vm": false
+        "events": require.resolve("events/") 
     }
 };
 
